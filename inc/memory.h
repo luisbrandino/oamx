f0709926432d95ed41be9b6cc072470938dbb355
luisbrandino <luisbrandino.contato@gmail.com>
2025-10-01 01:33:09 -0300

    chore: transform all I/O memory registers into members of the memory struct

--- inc/memory.h
+++ inc/memory.h
@@ -2,8 +2,25 @@
 #define MEMORY_H
 
 #include <stdint.h>
-
 #include "mbc.h"
+
+#define DIV_ADDR 0xFF04
+#define TIMA_ADDR 0xFF05
+#define TMA_ADDR 0xFF06
+#define TAC_ADDR 0xFF07
+#define LCDC_ADDR 0xFF40
+#define STAT_ADDR 0xFF41
+#define LY_ADDR 0xFF44
+#define LYC_ADDR 0xFF45
+#define SCY_ADDR 0xFF42
+#define SCX_ADDR 0xFF43
+#define BGP_ADDR 0xFF47
+#define OBP0_ADDR 0xFF48
+#define OBP1_ADDR 0xFF49
+#define WY_ADDR 0xFF4A
+#define WX_ADDR 0xFF4B
+#define IE_ADDR 0xFFFF
+#define IF_ADDR 0xFF0F
 
 typedef struct Memory {
     uint8_t rom[0x200000]; // 2MB: covers all possible 128 ROM banks for MBC1 (5-bit ROM bank + 2-bit RAM bank in ROM mode)
@@ -15,7 +32,25 @@ typedef struct Memory {
     uint8_t oam[0xA0];
     uint8_t io[0x80];   
     uint8_t hram[0x7F];
-    uint8_t ie;
+
+    // I/O registers
+    uint8_t div;
+    uint8_t tima;
+    uint8_t tma;
+    uint8_t tac;
+    uint8_t lcdc;
+    uint8_t stat;
+    uint8_t ly;
+    uint8_t lyc;
+    uint8_t scy;
+    uint8_t scx;
+    uint8_t bgp;
+    uint8_t obp0;
+    uint8_t obp1;
+    uint8_t wy;
+    uint8_t wx;
+    uint8_t IE;
+    uint8_t IF;
 
     MBC mbc;
 } Memory;
--- src/memory.c
+++ src/memory.c
@@ -1,36 +1,72 @@
 #include "../inc/memory.h"
 #include <stdlib.h>
 #include <string.h>
-
-static int can_access_vram(Memory* mem)
-{
-    uint8_t lcdc = mem->io[0x40];
-    uint8_t stat = mem->io[0x41];
-
-    uint8_t lcd_enabled = lcdc & (1 << 7);
-    uint8_t lcd_mode = stat & 0x03;
-
-    return lcd_enabled == 0x80 && (lcd_mode != 0x03);
-}
-
-static int can_access_oam(Memory* mem)
-{
-    uint8_t stat = mem->io[0x41];
-    uint8_t lcd_mode = stat & 0x03;
-
-    return lcd_mode != 0x02 && lcd_mode != 0x03;
-}
+#include <stdio.h>
 
 void memory_write(Memory* mem, uint16_t addr, uint8_t value)
 {
+    switch (addr)
+    {
+        case DIV_ADDR:
+            mem->div = value;
+            break;
+        case TIMA_ADDR:
+            mem->tima = value;
+            break;
+        case TMA_ADDR:
+            mem->tma = value;
+            break;
+        case TAC_ADDR:
+            mem->tac = value;
+            break;
+        case LCDC_ADDR:
+            mem->lcdc = value;
+            break;
+        case STAT_ADDR:
+            mem->stat = value;
+            break;
+        case LY_ADDR:
+            mem->ly = value;
+            break;
+        case LYC_ADDR:
+            mem->lyc = value;
+            break;
+        case SCY_ADDR:
+            mem->scy = value;
+            break;
+        case SCX_ADDR:
+            mem->scx = value;
+            break;
+        case BGP_ADDR:
+            mem->bgp = value;
+            break;
+        case OBP0_ADDR:
+            mem->obp0 = value;
+            break;
+        case OBP1_ADDR:
+            mem->obp1 = value;
+            break;
+        case WY_ADDR:
+            mem->wy = value;
+            break;
+        case WX_ADDR:
+            mem->wx = value;
+            break;
+        case IE_ADDR:
+            mem->IE = value;
+            break;
+        case IF_ADDR:
+            mem->IF = value;
+            break;
+    }
+
     if (addr <= 0x7FFF)
     {
         mem->mbc.rom_write(mem, addr, value);
     }
     else if (addr <= 0x9FFF)
     {
-        if (can_access_vram(mem))
-            mem->vram[addr - 0x8000] = value;
+        mem->vram[addr - 0x8000] = value;
     }
     else if (addr <= 0xBFFF)
     {
@@ -58,8 +94,7 @@ void memory_write(Memory* mem, uint16_t addr, uint8_t value)
     }
     else if (addr <= 0xFE9F)
     {
-        if (can_access_oam(mem))
-            mem->oam[addr - 0xFE00] = value;
+        mem->oam[addr - 0xFE00] = value;
     }
     else if (addr <= 0xFEFF)
     {
@@ -67,31 +102,69 @@ void memory_write(Memory* mem, uint16_t addr, uint8_t value)
     }
     else if (addr <= 0xFF7F)
     {
+        if (addr == 0xFF46)
+        {
+            mem->io[0x46] = value;
+            for (uint16_t i = 0; i < 0xA0; i++)
+                mem->oam[i] = memory_read(mem, (value << 8) + i);
+            return;
+        }
+
         mem->io[addr - 0xFF00] = value;
     }
     else if (addr <= 0xFFFE)
     {
         mem->hram[addr - 0xFF80] = value;
     }
-    else if (addr == 0xFFFF)
-    {
-        mem->ie = value;
-    }
 }
 
 uint8_t memory_read(Memory* mem, uint16_t addr)
 {
+    switch (addr)
+    {
+        case DIV_ADDR:
+            return mem->div;
+        case TIMA_ADDR:
+            return mem->tima;
+        case TMA_ADDR:
+            return mem->tma;
+        case TAC_ADDR:
+            return mem->tac;
+        case LCDC_ADDR:
+            return mem->lcdc;
+        case STAT_ADDR:
+            return mem->stat;
+        case LY_ADDR:
+            return mem->ly;
+        case LYC_ADDR:
+            return mem->lyc;
+        case SCY_ADDR:
+            return mem->scy;
+        case SCX_ADDR:
+            return mem->scx;
+        case BGP_ADDR:
+            return mem->bgp;
+        case OBP0_ADDR:
+            return mem->obp0;
+        case OBP1_ADDR:
+            return mem->obp1;
+        case WY_ADDR:
+            return mem->wy;
+        case WX_ADDR:
+            return mem->wx;
+        case IE_ADDR:
+            return mem->IE;
+        case IF_ADDR:
+            return mem->IF;
+    }
+
     if (addr <= 0x7FFF)
     {
         return mem->mbc.rom_read(mem, addr);
     }
     else if (addr <= 0x9FFF)
     {
-        // if this ever evolves to CGB, vram has switchable banks 0/1
-        if (can_access_vram(mem))
-            return mem->vram[addr - 0x8000];
-    
-        return 0xFF;
+        return mem->vram[addr - 0x8000];
     }
     else if (addr <= 0xBFFF)
     {
@@ -117,10 +190,7 @@ uint8_t memory_read(Memory* mem, uint16_t addr)
     }
     else if (addr <= 0xFE9F)
     {
-        if (can_access_oam(mem))
-            return mem->oam[addr - 0xFE00];
-
-        return 0xFF;
+        return mem->oam[addr - 0xFE00];
     }
     else if (addr <= 0xFEFF)
     {
@@ -128,15 +198,14 @@ uint8_t memory_read(Memory* mem, uint16_t addr)
     }
     else if (addr <= 0xFF7F)
     {
+        if (addr == 0xFF00)
+            return 0xCF; 
+
         return mem->io[addr - 0xFF00];
     }
     else if (addr <= 0xFFFE)
     {
         return mem->hram[addr - 0xFF80];
-    }
-    else if (addr == 0xFFFF)
-    {
-        return mem->ie;
     }
 
     return 0xFF;
@@ -156,10 +225,21 @@ uint16_t memory_read16(Memory* mem, uint16_t addr)
     return (hi << 8) | lo;
 }
 
+void memory_reset(Memory* mem)
+{
+    mem->div = 0x18;
+    mem->lcdc = 0x91;
+    mem->stat = 0x85;
+    mem->bgp = 0xFC;
+    mem->IF = 0xE1;
+}
+
+
 Memory* memory_init()
 {
     Memory *mem = (Memory*) malloc(sizeof(Memory));     
     memset(mem, 0, sizeof(Memory));
+    memory_reset(mem);
 
     set_mbc_type(mem, MBC1);
 
@@ -168,26 +248,21 @@ Memory* memory_init()
 
 void set_mbc_type(Memory* mem, MBCType type)
 {
-    if (mem->mbc.mbc_type == type)
-        return;
-
     switch (type)
     {
-        case MBC_NONE: {
+        case MBC_NONE:
             mem->mbc.mbc_type = MBC_NONE;
             mem->mbc.rom_write = rom_write_mbc_none;
             mem->mbc.rom_read = rom_read_mbc_none;
             mem->mbc.ram_write = ram_write_mbc_none;
             mem->mbc.ram_read = ram_read_mbc_none;
             break;
-        }
-        case MBC1: {
+        case MBC1:
             mem->mbc.mbc_type = MBC1;
             mem->mbc.rom_write = rom_write_mbc1;
             mem->mbc.rom_read = rom_read_mbc1;
             mem->mbc.ram_write = ram_write_mbc1;
             mem->mbc.ram_read = ram_read_mbc1;
             break;
-        }
     }
 }
